8b7ddb06606c464a364d7ade465e6e7cfe7c7769
Shohei Jinno <shohei.jinno@gmail.com>
2021-11-10 09:46:58 -0800

    [problem-set-01] Done with Luhn's algorithm.

--- problem-sets/src/bin/problem-set-01.rs
+++ problem-sets/src/bin/problem-set-01.rs
@@ -1,6 +1,13 @@
+use std::fmt::{self, Display};
 use std::io::{self, Write};
 
 fn main() {
+    // mario();
+    println!("{}", luhn());
+}
+
+#[allow(dead_code)]
+fn mario() {
     let height: u8;
 
     loop {
@@ -32,4 +39,102 @@ fn build_pyramid(height: usize) {
         }
         println!("{}", level.iter().collect::<String>());
     }
+}
+
+#[allow(dead_code)]
+enum CreditCard {
+    Amex,
+    Master,
+    Visa,
+    Invalid,
+}
+
+impl Display for CreditCard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            CreditCard::Amex => f.write_str("AMEX"),
+            CreditCard::Master => f.write_str("MASTER"),
+            CreditCard::Visa => f.write_str("VISA"),
+            CreditCard::Invalid => f.write_str("INVALID"),
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn luhn() -> CreditCard {
+    let mut number: Vec<u32>;
+    let mut card_type: CreditCard;
+
+    loop {
+        print!("Number: ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Faileed to read line");
+
+        number = input
+            .trim()
+            .chars()
+            .filter(|c| c.is_numeric())
+            .map(|c| c.to_digit(10).unwrap())
+            .collect();
+
+        card_type = if number.len() > 1 {
+            detect_card_type(&number[0], &number[1])
+        } else {
+            continue;
+        };
+
+        match (&card_type, number.len()) {
+            (&CreditCard::Amex, 15) => break,
+            (&CreditCard::Master, 16) => break,
+            (&CreditCard::Visa, 13 | 16 | 19) => break,
+            _ => continue,
+        };
+    }
+
+    if validate(number) {
+        match card_type {
+            CreditCard::Amex | CreditCard::Master | CreditCard::Visa => card_type,
+            _ => CreditCard::Invalid,
+        }
+    } else {
+        CreditCard::Invalid
+    }
+}
+
+fn detect_card_type(first: &u32, second: &u32) -> CreditCard {
+    match *first {
+        4 => CreditCard::Visa,
+        5 => match *second {
+            (1..=5) => CreditCard::Master,
+            _ => CreditCard::Invalid,
+        },
+        3 => match *second {
+            4 | 7 => CreditCard::Amex,
+            _ => CreditCard::Invalid,
+        },
+        _ => CreditCard::Invalid,
+    }
+}
+
+fn validate(number: Vec<u32>) -> bool {
+    let is_odd = number.len() % 2 == 1;
+    let rem = if is_odd { 1 } else { 0 };
+
+    number.iter().enumerate().fold(0, |acc, (idx, num)| {
+        if idx % 2 == rem {
+            let mul = 2 * num;
+            if mul > 9 {
+                acc + 1 + mul % 10
+            } else {
+                acc + mul
+            }
+        } else {
+            acc + num
+        }
+    }) % 10
+        == 0
 }
