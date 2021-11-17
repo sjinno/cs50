3edbe2aed0a2766cc1e43c9bbf52af253696e37c
Shohei Jinno <shohei.jinno@gmail.com>
2021-11-17 15:06:27 -0800

    [lab-04] Improve a little.

--- labs/src/bin/lab-04.rs
+++ labs/src/bin/lab-04.rs
@@ -1,44 +1,36 @@
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use failure::Error;
+use labs::input;
 use std::fs::File;
-use std::io::{BufReader, Cursor, Read, Write};
+use std::io::{Cursor, Read, Write};
 
 const HEADER_SIZE: usize = 44;
 
 fn main() -> Result<(), Error> {
-    let factor = 2.0;
+    // Create the output buffer
+    let mut output_buf = File::create("audio/output.wav")?;
+    let factor = 2.5;
 
     // Open file
-    let file = File::open("audio/input.wav")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut data = Vec::new();
-    let _ = buf_reader.read_to_end(&mut data)?;
+    let mut buf_reader = input::get_reader("audio/input.wav")?;
 
     // Read the first 44 bytes which is the header of the WAV file
-    let header = &data[..HEADER_SIZE];
-    println!("{:?}", header);
-    eprintln!("{}", String::from_utf8_lossy(&header));
+    let mut header = vec![0; HEADER_SIZE];
+    buf_reader.read_exact(&mut header)?;
+
+    // Write to the output file
+    output_buf.write_all(&header)?;
 
     // Read the rest of the file each sample being 16 bits (2 bytes)
     // and write it to the output file
-    eprintln!("{}", &data[44..].len());
-    // eprintln!("{:?}", &data[44..4044]);
     let mut samples = Vec::new();
-    let mut rdr = Cursor::new(&data[44..]);
-    loop {
-        match rdr.read_i16::<LittleEndian>() {
-            Ok(v) => samples.write_i16::<LittleEndian>(adjust_volume(v, factor))?,
-            Err(_) => break,
-        }
-    }
-    // eprintln!("{:?}", &samples[..4000]);
-    eprintln!("{}", samples.len());
+    buf_reader.read_to_end(&mut samples)?;
+    eprintln!("lenth of samples {}", samples.len());
 
-    let mut output_buf = File::create("audio/output.wav")?;
-    let mut header = header.to_owned();
-    header.extend_from_slice(&samples);
-    let bytes = output_buf.write(&header)?;
-    eprintln!("bytes written to file: {}", bytes);
+    let mut reader = Cursor::new(&samples);
+    while let Ok(val) = reader.read_i16::<LittleEndian>() {
+        output_buf.write_i16::<LittleEndian>(adjust_volume(val, factor))?
+    }
 
     Ok(())
 }
